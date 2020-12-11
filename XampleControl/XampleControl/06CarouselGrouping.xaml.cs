ce597d2ee15a0288fcab275f18c86dd85b69d36b
shawyunz <shawyunz@gmail.com>
2020-12-11 23:33:09 +1300

    :children_crossing: Add a raw list for comparison. Can add new item on the page.

--- XampleControl/XampleControl/06CarouselGrouping.xaml.cs
+++ XampleControl/XampleControl/06CarouselGrouping.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -11,91 +12,105 @@ namespace XampleControl
 		public _06CarouselGrouping()
 		{
 			InitializeComponent();
-			MockGrouping = MockColorList
-				.GroupBy(u => u.Type)
-				.Select(grp => new ColorGroup(){
-					ColorCategory = grp.Key,
-					ColorList = grp.ToList()
-				})
-				.ToList();
 
+			MockColorList = new ObservableCollection<ColorBox>()
+			{
+				new ColorBox {
+					Type = "red",
+					Code = "#fbe2ea"
+				},
+				new ColorBox {
+					Type = "red",
+					Code = "#f7bcd0"
+				},
+				new ColorBox {
+					Type = "red",
+					Code = "#fb82ab"
+				},
+				new ColorBox {
+					Type = "red",
+					Code = "#fa4482"
+				},
+				new ColorBox {
+					Type = "red",
+					Code = "#c01e5c"
+				},
+				new ColorBox {
+					Type = "red",
+					Code = "#86134f"
+				},
+
+				new ColorBox {
+					Type = "green",
+					Code = "#aafeeb"
+				},
+				new ColorBox {
+					Type = "green",
+					Code = "#34e8b7"
+				},
+				new ColorBox {
+					Type = "green",
+					Code = "#2abea5"
+				},
+				new ColorBox {
+					Type = "green",
+					Code = "#1e838e"
+				},
+				new ColorBox {
+					Type = "green",
+					Code = "#136063"
+				},
+
+				new ColorBox {
+					Type = "purple",
+					Code = "#8d2ca8"
+				},
+				new ColorBox {
+					Type = "purple",
+					Code = "#5e3aae"
+				},
+				new ColorBox {
+					Type = "purple",
+					Code = "#7c54fb"
+				}
+			};
 			BindingContext = this;
 		}
 
-		public List<ColorBox> MockColorList => new List<ColorBox>()
+		public ObservableCollection<ColorGroup> MockColorGrouping =>
+			new ObservableCollection<ColorGroup>(
+				MockColorList
+				.GroupBy(u => u.Type)
+				.Select(grp => new ColorGroup()
+				{
+					ColorCategory = grp.Key,
+					ColorList = grp.ToList()
+				})
+			);
+
+		public ObservableCollection<ColorBox> MockColorList { get; set; }
+
+		private void Button_Clicked(object sender, System.EventArgs e)
 		{
-			new ColorBox {
-				Type = "red",
-				Code = "#fbe2ea"
-			},
-			new ColorBox {
-				Type = "red",
-				Code = "#f7bcd0"
-			},
-			new ColorBox {
-				Type = "red",
-				Code = "#fb82ab"
-			},
-			new ColorBox {
-				Type = "red",
-				Code = "#fa4482"
-			},
-			new ColorBox {
-				Type = "red",
-				Code = "#c01e5c"
-			},
-			new ColorBox {
-				Type = "red",
-				Code = "#86134f"
-			},
+			MockColorList.Add(
+				new ColorBox
+				{
+					Type = TxtGroup.Text,
+					Code = TxtCode.Text
+				});
+			OnPropertyChanged(nameof(MockColorGrouping));
+		}
 
-			new ColorBox {
-				Type = "green",
-				Code = "#aafeeb"
-			},
-			new ColorBox {
-				Type = "green",
-				Code = "#34e8b7"
-			},
-			new ColorBox {
-				Type = "green",
-				Code = "#2abea5"
-			},
-			new ColorBox {
-				Type = "green",
-				Code = "#1e838e"
-			},
-			new ColorBox {
-				Type = "green",
-				Code = "#136063"
-			},
+		public class ColorBox
+		{
+			public string Code { get; set; }
+			public string Type { get; set; }
+		}
 
-			new ColorBox {
-				Type = "purple",
-				Code = "#8d2ca8"
-			},
-			new ColorBox {
-				Type = "purple",
-				Code = "#5e3aae"
-			},
-			new ColorBox {
-				Type = "purple",
-				Code = "#7c54fb"
-			}
-		};
-
-		public List<ColorGroup> MockGrouping { get; set; }
-	}
-
-	public class ColorBox
-	{
-		public string Code { get; set; }
-		public string Type { get; set; }
-	}
-
-	public class ColorGroup
-	{
-		public string ColorCategory { get; set; }
-		public List<ColorBox> ColorList { get; set; }
+		public class ColorGroup
+		{
+			public string ColorCategory { get; set; }
+			public List<ColorBox> ColorList { get; set; }
+		}
 	}
 }
