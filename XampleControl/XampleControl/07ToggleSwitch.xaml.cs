19e4e6996151f00fd31233edb6a496ee1d332e88
shawyunz <shawyunz@gmail.com>
2021-04-03 00:12:14 +1300

    :dizzy: Add animation and align the color and state of the switch to common ones.

--- XampleControl/XampleControl/07ToggleSwitch.xaml.cs
+++ XampleControl/XampleControl/07ToggleSwitch.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,27 +8,42 @@ namespace XampleControl
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class _07ToggleSwitch : ContentPage
 	{
+		private bool isBusy = false;
+
 		public _07ToggleSwitch()
 		{
 			InitializeComponent();
 			BindingContext = this;
 		}
 
-		public bool IsSelected { get; set; } = true;
+		public bool IsSelected { get; set; } = false;
 
-		private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+		private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
 		{
-			if (IsSelected)
+			if (isBusy)
 			{
-				await SwitchEllips.TranslateTo(32, 0, 120, Easing.CubicInOut);
+				return;
+			}
+
+			isBusy = true;
+			if (!IsSelected)
+			{
+				Task.WhenAll(
+					SwitchEllips.TranslateTo(32, 0, 120, Easing.CubicInOut),
+					EllipsBackground.ScaleTo(30, 200)
+				);
 			}
 			else
 			{
-				await SwitchEllips.TranslateTo(0, 0, 110, Easing.CubicInOut);
+				Task.WhenAll(
+					SwitchEllips.TranslateTo(0, 0, 110, Easing.CubicInOut),
+					EllipsBackground.ScaleTo(1, 200)
+				);
 			}
 
 			IsSelected = !IsSelected;
 			OnPropertyChanged(nameof(IsSelected));
+			isBusy = false;
 		}
 	}
 }
